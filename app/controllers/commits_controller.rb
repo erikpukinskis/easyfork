b3b45fb63f0be1039e7d5411a948cbe2538d4f90
Erik Pukinskis <erik@x60erik.(none)>
2009-10-22 15:04:47 -0700

    fixed undo slider

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -2,6 +2,6 @@ class CommitsController < ApplicationController
   def show
     app = App.find_by_id(params[:app_id])
     sha = app.commits[params[:id].to_i]["sha"]
-    render :text => app.old_code(sha)
+    render :json => {:text => app.old_code(sha), :index => params[:id].to_i}
   end
 end
--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -3,6 +3,6 @@ class FilesController < ApplicationController
     app = App.find_by_id(params[:app_id])
     require_owner(app, current_user)
     app.save_file(params[:id], params[:app_code])
-    render :json => {:status => :ok}
+    render :json => {:status => :ok, :commit_count => app.commits.length}
   end
 end
