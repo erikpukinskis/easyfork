202a43769916a1195d0cf5bf74a79beaccc6fd4b
Erik Pukinskis <erik@x60erik.(none)>
2009-11-06 15:43:48 -0800

    remove debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,7 +55,7 @@ class ApplicationController < ActionController::Base
     def require_user
       unless current_user
         store_location
-        flash[:notice] = "You must be logged in to access this page" << current_user.inspect
+        flash[:notice] = "You must be logged in to access this page"
         redirect_to new_user_session_url
         return false
       end
