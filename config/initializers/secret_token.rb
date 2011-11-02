6a494dbe624c993db379829d2e72e5294f0dad23
Erik Pukinskis <erik@snowedin.net>
2011-11-01 23:23:30 -0700

    Migrated to Rails 3.1 without testing anything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easyfork::Application.config.secret_token = '0daf5ad03625eefdf816c5763afb0a2aec9151bf3e6ad99fdce86ef013be582185f62c57c83de726bc702640dbd295ce029d636550f6713deb474b45f98a1efe'
--- config/routes.rb
+++ config/routes.rb
@@ -1,76 +1,32 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :invite_requests
-  map.connect '/thanks', :controller => "site", :action => "thanks"
-
-  map.resources :invites
-
-  map.root :controller => "site", :action => "home"
-
+Easyfork::Application.routes.draw do
+  resources :invite_requests
+  resources :invites
+  resource :user_session
+  resources :apps, :member => [:name]
+  #map.resources :users
   #map.resources :apps, :member => [:fork, :deploy] do |app|
   #  app.resources :files, :requirements => { :id => /.*/ }
   #  app.resources :commits, :only => [:show, :create, :index]
   #end
-  map.resource :user_session
-  map.join '/join', :controller => "users", :action => "new",  :conditions => { :method => :get }
-  map.connect '/join', :controller => "users", :action => "create",  :conditions => { :method => :post }
-  map.connect '/signin', :controller => "user_sessions", :action => "new"
-  map.connect '/signout', :controller => "user_sessions", :action => "destroy"
-  #map.resources :users
 
-  #map.apps '/apps', :controller => "apps", :action => "create", :conditions => { :method => :post }
-  map.resources :apps, :member => [:name]
-  map.connect ':login', :controller => "users", :action => "show"
-  map.connect ':owner_id/:name.:format', :controller => "apps", :action => "show"
+  get 'thanks' => "site#thanks"
+  get 'join' => "users#new"
+  post 'join' => "users#create"
+  get 'signin' => "user_sessions#new"
+  get 'signout' => "user_sessions#destroy"
+  get 'login' => "users#show"
 
-  map.connect ':owner_id/:name/fork', :controller => "apps", :action => "fork"
-  map.connect ':owner_id/:name/loading', :controller => "apps", :action => "loading"
-  map.connect ':owner_id/:name/deploy', :controller => "apps", :action => "deploy"
-
-  map.connect ':owner_id/:name/commits', :controller => "commits", :action => "index", :conditions => { :method => :get }
-  map.connect ':owner_id/:name/commits', :controller => "commits", :action => "create", :conditions => { :method => :post }
-  map.connect ':owner_id/:name/commits/:index', :controller => "commits", :action => "show"
-  map.connect ':owner_id/:name/files/:filename', :controller => "files", :action => "update", :conditions => { :method => :put }, :requirements => { :filename => /.*/ }
-  #map.user ':id', :controller => "users", :action => "show"
-  #map.app ':owner_id/:id', :controller => "apps", :action => "show"
-
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
+  scope ":owner_id/:name" do
+    get "fork" => "apps#fork"
+    get "loading" => "apps#loading"
+    get "deploy" => "apps#deploy"
   
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
+    get "commits" => "commits#index"
+    post "commits" => "commits#create"
+    get "commits/:index" => "commits#show"
 
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+    put "/files/:filename" => "files#update", :constraints => { :filename => /.*/ }
+  end
+  
+  root :to => "site#home"
 end
